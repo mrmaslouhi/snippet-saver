9db6087595df794817c966547de823b041a3d667
Maslouhi <coconutcrab250@gmail.com>
2025-06-09 20:03:36 +0100

    Added textarea and code highlighting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,37 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/themes/prism.css';
+import prismLanguages from './prismLanguages';
+import './style.css'
 
 
-function App() {
-  return "Hello World"
+const App = () => {
+  const [code, setCode] = useState(
+    `console.log("Hello, World!")`
+  )
+
+  return (
+    <>
+      <label htmlFor='lang'>Language</label>
+      <select name='lang' id='lang'>
+        {Object.entries(prismLanguages).map()}
+      </select>
+      <Editor
+        className='code-input'
+        value={code}
+        onValueChange={code => setCode(code)}
+        highlight={code => highlight(code, languages.markup)}
+        padding={12}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+        }}
+      />
+    </>
+
+  );
 }
 
 export default App
