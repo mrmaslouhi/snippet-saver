329320eb9c5c80912554b84ac01008e413124948
mrmaslouhi <anasmmvc@gmail.com>
2025-06-13 08:13:28 -0700

    Show snippets in /snippets

--- frontend/src/Components/SnippetsPage.jsx
+++ frontend/src/Components/SnippetsPage.jsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react'
+import snippetService from '../services/snippets'
+
 const SnippetsPage = () => {
+    const [snippets, setSnippets] = useState([])
+    useEffect(() => {
+        const getSnippets = async () => {
+            const data = await snippetService.getAllSnippets()
+            setSnippets(data)
+        } // AI Code
+        getSnippets()
+    }, [])
+
     return (
         <>
             <h1>Welcome, User</h1>
             <input type="text" placeholder="Search Your Snippets" />
             <div className="snippet-container">
                 <div className="snippet-title">Title</div>
-                <div className="snippet">console.log('hello, world')</div>
+                {snippets.map(snippet => 
+                    <div key={snippet.id} style={{ padding: 5, border: 1, borderStyle: 'solid', borderColor: 'black', fontFamily: 'monospace' }} className="snippet">
+                        {snippet.title}
+                        {snippet.code}
+                    </div>
+                )}
             </div>
         </>
     )
